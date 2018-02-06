eba5c65aab7a8fd7a7532c33de9a797e4fa08043
J_Ford <jessief423@gmail.com>
2018-02-05 22:15:51 -0500

    fixed CORS access header

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ var app = express();
 var port = process.env.PORT || 3000;
 var stockRouter = express.Router();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 stockRouter.route('/stocks')
    .post(function(req, res){
         var stock = new Stock(req.body);
@@ -45,3 +54,4 @@ app.get('/', function(req, res){
 app.listen(port, function(){
     console.log('running on port ' + port)
 });
+
